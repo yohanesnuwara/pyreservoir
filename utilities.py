57ad22d974f1f9e54d4ea686511051a8832cf1f1
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-06-25 19:43:15 +0700

    update nomenclatures.py

--- utilities.py
+++ utilities.py
@@ -1,21 +1,25 @@
 def nomenclatures(parameter):
     """
     Dictionary for inputs and outputs, giving the descriptions, and the oilfield units
-    
+
     Input:
     parameter = the input-output parameter, string
-    
+
     Output:
     description = the description of the parameter
     unit = the oilfield unit of the parameter
     """
-    description = {"Bg": "gas formation volume factor", "Bo": "oil FVF", "Bw": "water FVF"}
-    unit = {"Bg": "RB/scf", "Bo": "RB/STB", "Bw": "RB/STB"}
+    description = {"Bg": "gas formation volume factor",
+                   "Bo": "oil formation volume factor",
+                   "Bw": "water formation volume factor"}
+    unit = {"Bg": "RB/scf",
+            "Bo": "RB/STB",
+            "Bw": "RB/STB"}
 
     description = description[parameter]
     unit = unit[parameter]
 
-    return(description, unit)
+    return (description, unit)
 
 # testing
-print(nomenclatures("Bg"))
+print(nomenclatures("Bw"))
