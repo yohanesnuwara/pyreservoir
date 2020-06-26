c942954e6348ce4217cfbfda17e522f3dbb922e9
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-06-26 13:34:50 +0700

    Update gascorrelation.py

--- pvt/gascorrelation.py
+++ pvt/gascorrelation.py
@@ -1,1 +1,10 @@
+"""
+Correlation Models for Gas Properties
 
+@author: Yohanes Nuwara
+@email: ign.nuwara97@gmail.com
+"""
+
+class compressibility_factor():
+  def dranchuk_aboukassem():
+    return(z)
