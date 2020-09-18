142e9641e8fe4f2c007ac4c5165b3b9a10144de4
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-09-19 00:29:58 +0700

    update pvtcorrelation

--- pvt/pvtcorrelation.py
+++ pvt/pvtcorrelation.py
@@ -365,6 +365,15 @@ def gasoilratio(pressure2, P_bubble, sg2, api, temp2, Rsb):
 WATER
 """
 
+def water_fvf(temp, p):
+  "Water FVF (Bw)"
+  # temp in Fahrenheit
+  # p pressure in psia
+  Vwp = (-1.95301E-9 * p * temp) - (1.72834E-13 * (p**2) * temp) - (3.588922E-7 * p) - (2.25341E-10 * p**2)
+  Vwt = (-1.001E-2) + (1.33391E-4 * temp) + (5.50654E-7 * temp**2)
+  Bw = (1 + Vwt) * (1 + Vwp)
+  return(Bw)
+
 def water_pbubble(temp):
   """
   Calculate Vapour (Bubble Point) Pressure of Water
