a87acfebdc1fef1f8e8e6415ce8be8d2f42ff9b8
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2021-02-19 13:53:10 +0700

    Add plotting to arps_fit

--- dca/dca.py
+++ dca/dca.py
@@ -22,6 +22,7 @@ def arps_fit(t, q):
     return np.sqrt(np.sum(y-yfit)**2 / N)
 
   # subtract one datetime to another datetime
+  date = t
   timedelta = [j-i for i, j in zip(t[:-1], t[1:])]
   timedelta = np.array(timedelta)
   timedelta = timedelta / datetime.timedelta(days=1)
@@ -46,5 +47,29 @@ def arps_fit(t, q):
   # De-normalize qi and di
   qi = qi * max(q)
   di = di / max(t)
+
+  # Print all parameters and RMSE
+  print('Initial production rate (qi)  : {:.5f} SCF'.format(qi))
+  print('Initial decline rate (di)     : {:.5f} SCF/D'.format(di))
+  print('Decline coefficient (b)       : {:.5f}'.format(b))
+  print('RMSE of regression            : {:.5f}'.format(RMSE))  
+
+  # Produce the hyperbolic curve (fitted)
+  tfit = np.linspace(min(t), max(t), 100)
+  qfit = hyperbolic(tfit, qi, di, b)
+
+  # Plot data and hyperbolic curve
+  plt.figure(figsize=(10,7))
+
+  plt.step(t, q, color='blue', label="Data")
+  plt.plot(tfit, qfit, color='red', label="Hyperbolic Curve")
+  plt.title('Decline Curve Analysis', size=20, pad=15)
+  plt.xlabel('Days')
+  plt.ylabel('Rate (SCF/d)')
+  plt.xlim(min(t), max(t)); plt.ylim(ymin=0)
+
+  plt.legend()
+  plt.grid()
+  plt.show()
 
   return qi, di, b, RMSE
