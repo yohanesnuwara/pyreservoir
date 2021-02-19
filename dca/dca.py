7137897d538067e57c8fb72f827cfdb1fc89ab53
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2021-02-19 17:39:57 +0700

    add plotting option to arps_fit

--- dca/dca.py
+++ dca/dca.py
@@ -5,7 +5,7 @@ def hyperbolic(t, qi, di, b):
   import numpy as np
   return qi / (np.abs((1 + b * di * t))**(1/b))
 
-def arps_fit(t, q):
+def arps_fit(t, q, maxfev=800, plot=None):
   """
   Arps Decline Curve Analysis using Non-Linear Curve-Fitting
   
@@ -21,8 +21,8 @@ def arps_fit(t, q):
   import datetime
   from scipy.optimize import curve_fit
 
-#   def hyperbolic(t, qi, di, b):
-#     return qi / (np.abs((1 + b * di * t))**(1/b))
+  def hyperbolic(t, qi, di, b):
+    return qi / (np.abs((1 + b * di * t))**(1/b))
   
   def rmse(y, yfit):
     N = len(y)
@@ -44,7 +44,7 @@ def arps_fit(t, q):
   q_normalized = q / max(q)  
 
   # fitting the data with the hyperbolic function
-  popt, pcov = curve_fit(hyperbolic, t_normalized, q_normalized)
+  popt, pcov = curve_fit(hyperbolic, t_normalized, q_normalized, maxfev=maxfev)
   qi, di, b = popt
 
   # RMSE is calculated on the normalized variables
@@ -55,29 +55,30 @@ def arps_fit(t, q):
   qi = qi * max(q)
   di = di / max(t)
 
-  # Print all parameters and RMSE
-  print('Initial production rate (qi)  : {:.5f} SCF'.format(qi))
-  print('Initial decline rate (di)     : {:.5f} SCF/D'.format(di))
-  print('Decline coefficient (b)       : {:.5f}'.format(b))
-  print('RMSE of regression            : {:.5f}'.format(RMSE))  
+  if plot==True:
+    # Print all parameters and RMSE
+    print('Initial production rate (qi)  : {:.5f} SCF'.format(qi))
+    print('Initial decline rate (di)     : {:.5f} SCF/D'.format(di))
+    print('Decline coefficient (b)       : {:.5f}'.format(b))
+    print('RMSE of regression            : {:.5f}'.format(RMSE))  
 
-  # Produce the hyperbolic curve (fitted)
-  tfit = np.linspace(min(t), max(t), 100)
-  qfit = hyperbolic(tfit, qi, di, b)
+    # Produce the hyperbolic curve (fitted)
+    tfit = np.linspace(min(t), max(t), 100)
+    qfit = hyperbolic(tfit, qi, di, b)
 
-  # Plot data and hyperbolic curve
-  plt.figure(figsize=(10,7))
+    # Plot data and hyperbolic curve
+    plt.figure(figsize=(10,7))
 
-  plt.step(t, q, color='blue', label="Data")
-  plt.plot(tfit, qfit, color='red', label="Hyperbolic Curve")
-  plt.title('Decline Curve Analysis', size=20, pad=15)
-  plt.xlabel('Days')
-  plt.ylabel('Rate (SCF/d)')
-  plt.xlim(min(t), max(t)); plt.ylim(ymin=0)
+    plt.step(t, q, color='blue', label="Data")
+    plt.plot(tfit, qfit, color='red', label="Hyperbolic Curve")
+    plt.title('Decline Curve Analysis', size=20, pad=15)
+    plt.xlabel('Days')
+    plt.ylabel('Rate (SCF/d)')
+    plt.xlim(min(t), max(t)); plt.ylim(ymin=0)
 
-  plt.legend()
-  plt.grid()
-  plt.show()
+    plt.legend()
+    plt.grid()
+    plt.show()
 
   return qi, di, b, RMSE
 
