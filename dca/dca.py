93c501f140550845c80c924a59e512a22b5048b1
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2021-02-19 10:32:03 +0700

    Create dca.py

---
+++ dca/dca.py
@@ -0,0 +1,44 @@
+def arps_fit(t, q):
+  """
+  Arps Decline Curve Analysis using Non-Linear Curve-Fitting
+  
+  Input:
+  t = time array (in numpy datetime64)
+  q = production rate array (unit: STB/day, or SCF/day)
+
+  Output:
+  qi = initial production rate (unit: STB/day, or SCF/day)
+  di = initial decline rate (unit: STB/day, or SCF/day)
+  b = decline exponent 
+  """
+  import numpy as np
+  import datetime
+  from scipy.optimize import curve_fit
+
+  def hyperbolic(t, qi, di, b):
+    return qi / (np.abs((1 + b * di * t))**(1/b))
+
+  # subtract one datetime to another datetime
+  timedelta = [j-i for i, j in zip(t[:-1], t[1:])]
+  timedelta = np.array(timedelta)
+  timedelta = timedelta / datetime.timedelta(days=1)
+
+  # take cumulative sum over timedeltas
+  t = np.cumsum(timedelta)
+  t = np.append(0, t)
+  t = t.astype(float)
+
+  # normalize the time and rate data
+  t_normalized = t / max(t)
+  q_normalized = q / max(q)  
+
+  # fitting the data with the hyperbolic function
+  popt, pcov = curve_fit(hyperbolic, t_normalized, q_normalized)
+
+  qi, di, b = popt
+
+  # de-normalize qi and di
+  qi = qi * max(q)
+  di = di / max(t)
+
+  return qi, di, b
